6afcb14e643896c7b69d48b334a6d46b38d83c0c
Stephan Griesel <sgriesel@gmail.com>
2018-10-22 06:40:45 +0200

    connect to db, add promises

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(bodyParser.json());
 
 // DB Config 
 const db = require('./config/keys').mongoURI;
+
+// Connect DB
+mongoose
+.connect(db)
+.then(() => console.log('SUKSES!! Connected...'))
+.catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`SUKSES!! Server started on port ${port}`));
