47086147a2250d8a058416437838b41387307cb5
Stephan Griesel <sgriesel@gmail.com>
2018-10-23 06:14:51 +0200

    add items var

---
+++ routes/api/items.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Item model
+const Item = require('../../models/Item');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
+
+
 const app = express();
 
 //Body Parser 
@@ -15,6 +19,10 @@ mongoose
 .connect(db)
 .then(() => console.log('SUKSES!! Connected...'))
 .catch(err => console.log(err));
+
+// Routes
+
+app.use('api/items', items);
 
 const port = process.env.PORT || 5000;
 
