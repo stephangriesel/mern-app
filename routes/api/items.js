70f64d186b40546f1b4fb2782d00682b820a6d13
Stephan Griesel <sgriesel@gmail.com>
2018-10-23 06:18:42 +0200

    add route

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,4 +4,13 @@ const router = express.Router();
 // Item model
 const Item = require('../../models/Item');
 
+// @route GET api/items
+// @desc GET All Items
+// @access PUBLIC
+router.get('/', (req,res) => {
+    Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items))
+});
+
 module.exports = router;
