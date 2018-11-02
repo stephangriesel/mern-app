426a78efad2100a9661415210ebc2cbfeaf7180d
Stephan Griesel <sgriesel@gmail.com>
2018-11-02 07:11:38 +0100

    add item modal - bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import AppNavBar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
+import React, { Component } from "react";
+import AppNavBar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+        <div className="App">
+          <Container>
+            <AppNavBar />
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
       </Provider>
     );
   }
