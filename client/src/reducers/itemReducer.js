819b5eb0118678a20a2a58db07a294af10d8d8e6
Stephan Griesel <sgriesel@gmail.com>
2018-11-03 07:09:50 +0100

    add axios, connect backend

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,20 +1,18 @@
 import uuid from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Bread" },
-    { id: uuid(), name: "Butter" },
-    { id: uuid(), name: "Coffee" },
-    { id: uuid(), name: "Banana" }
-  ]
+  items: [],
+  loading: false
 }
 
 export default function(state = initialState, action){
     switch(action.type) {
         case GET_ITEMS:
         return {
-            ...state
+            ...state,
+            items: action.payload,
+            loading:false
         };
         case DELETE_ITEM:
         return {
@@ -26,6 +24,11 @@ export default function(state = initialState, action){
             ...state,
             items: [action.payload, ...state.items]
         };
+        case ITEMS_LOADING:
+        return {
+            ...state,
+            loading:true
+        } 
         default:
         return state;
         }
